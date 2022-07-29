302873571eb9765b2de991760e9dd45f5edadb47
artmakk <82980291+artmakk@users.noreply.github.com>
2022-07-29 23:46:31 +0000

    Initial commit

---
+++ bot.js/config.py
@@ -0,0 +1,7 @@
+settings = {
+    'TOKEN':
+    'ODMzNDQ0NDUxMDE1NDU4ODE3.G32ydF.rtfnEiNHbWDM96EoIoGoH5h79g4f9DLOX1fiTs',
+    'bot': 'Kavazumi',
+    'PREFIX': '#',
+    'OWNER NAME': 'artmakk#5001',
+}
